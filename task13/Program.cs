e57ceec60ad33977d7f5081d9f3f8bf4e6a2a4da
Sergey <sergey.malygin.93@mail.ru>
2022-06-27 23:51:54 +0300

    add task 13

---
+++ task13/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+int number = new Random().Next(1, 99999);
+
+if (number < 100)
+{
+    Console.WriteLine($"{number} -> третей цифры нет");
+}
+
+else if (number >= 100)
+{
+    string result = number.ToString();
+    Console.WriteLine($"{number} -> {result[2]}");
+}
